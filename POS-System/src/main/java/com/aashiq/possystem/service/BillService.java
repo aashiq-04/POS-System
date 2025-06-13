e83dd409b537b38e49ba7096be22990a37cdc762
aashiq-04 <mohd04aashiq@gmail.com>
2025-06-13 22:14:13 +0530

    Application properties updated

--- POS-System/src/main/java/com/aashiq/possystem/service/BillService.java
+++ POS-System/src/main/java/com/aashiq/possystem/service/BillService.java
@@ -8,7 +8,6 @@ import com.aashiq.possystem.repository.BillRepository;
 import com.aashiq.possystem.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
