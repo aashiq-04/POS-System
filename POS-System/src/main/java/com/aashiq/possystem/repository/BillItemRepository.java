679b2916a2fbbe0ab22f55278ac4acc93b752a60
aashiq-04 <mohd04aashiq@gmail.com>
2025-06-15 22:02:11 +0530

    Backend updates

---
+++ POS-System/src/main/java/com/aashiq/possystem/repository/BillItemRepository.java
@@ -0,0 +1,15 @@
+package com.aashiq.possystem.repository;
+
+import com.aashiq.possystem.model.BillItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BillItemRepository  extends JpaRepository<BillItem, Integer> {
+    @Query("SELECT i.product.name, SUM(i.quantity) FROM BillItem i WHERE i.bill.createdAt BETWEEN :start AND :end GROUP BY i.product.name ORDER BY SUM(i.quantity) DESC ")
+    List<Object[]> findTopSellingProducts(@Param("start")LocalDateTime start, @Param("end") LocalDateTime end);
+}
---
+++ POS-System/src/main/java/com/aashiq/possystem/service/AnalyticsService.java
@@ -0,0 +1,49 @@
+package com.aashiq.possystem.service;
+
+import com.aashiq.possystem.repository.BillItemRepository;
+import com.aashiq.possystem.repository.BillRepository;
+import com.aashiq.possystem.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AnalyticsService {
+    private BillRepository billRepository;
+    private BillItemRepository billItemRepository;
+
+    public Map<String, Object> getAnalytics(LocalDateTime start, LocalDateTime end)
+    {
+        Map<String, Object> result= new HashMap<>();
+        Double revenue = billRepository.getTotalRevenueBetweenDates(start, end);
+        Long billCount = billRepository.getBillCountBetweenDates(start, end);
+        List<Object[]> topProductsRaw = billItemRepository.findTopSellingProducts(start, end);
+
+        int totalItemSold = 0;
+        List<Map<String, Object>> topProducts = new ArrayList<>();
+
+        for(Object[] row: topProductsRaw)
+        {
+            String name = (String) row[0];
+            Long quantity = (Long) row[1];
+
+            Map<String, Object> item = new HashMap<>();
+            item.put("name", name);
+            item.put("quantitySold", quantity);
+            topProducts.add(item);
+
+            totalItemSold += quantity;
+        }
+
+        result.put("totalRevenue", revenue != null ? revenue : 0.0);
+        result.put("totalBills", billCount);
+        result.put("totalItemsSold", totalItemSold);
+        result.put("topProducts", topProducts);
+
+        return result;
+    }
+}
