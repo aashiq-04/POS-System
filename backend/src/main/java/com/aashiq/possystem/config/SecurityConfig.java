3bbf5d39e379eeae61425168fdde10353b40869f
aashiq-04 <mohd04aashiq@gmail.com>
2025-08-12 00:30:47 +0530

    Actuator check added

--- backend/src/main/java/com/aashiq/possystem/config/SecurityConfig.java
+++ backend/src/main/java/com/aashiq/possystem/config/SecurityConfig.java
@@ -2,23 +2,21 @@ package com.aashiq.possystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/health","/actuator/info").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable());
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic();
         return http.build();
     }
 }
