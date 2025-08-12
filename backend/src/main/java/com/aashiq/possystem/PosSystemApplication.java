601f055101aeee169aa6f1d78715afa7d90638f9
aashiq-04 <mohd04aashiq@gmail.com>
2025-08-12 20:16:08 +0530

    Fix DB Config for Render

--- backend/src/main/java/com/aashiq/possystem/PosSystemApplication.java
+++ backend/src/main/java/com/aashiq/possystem/PosSystemApplication.java
@@ -2,12 +2,21 @@ package com.aashiq.possystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class PosSystemApplication {
+public class PosSystemApplication implements CommandLineRunner {
+
+    @Value("${server.port}")
+    private String serverPort;
 
     public static void main(String[] args) {
         SpringApplication.run(PosSystemApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("Server is running on port: " + serverPort);
+    }
 }
